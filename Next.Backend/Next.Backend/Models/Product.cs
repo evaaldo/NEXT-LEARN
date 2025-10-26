5642bdad0ac0ae6e534bdbdddef5c88b703917d3
Evaldo Rodrigues <110418142+evaaldo@users.noreply.github.com>
2025-10-26 14:29:56 -0300

    Delete Next.Backend directory

--- Next.Backend/Next.Backend/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace Next.Backend.Models;
-
-public class Product
-{
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public decimal Price { get; set; }
-    public required string Supplier { get; set; }
-}
--- Next.Backend/Next.Backend/Services/Interfaces/IProductService.cs
+++
@@ -1,8 +0,0 @@
-using Next.Backend.Models;
-
-namespace Next.Backend.Services.Interfaces;
-
-public interface IProductService
-{
-    public Task<IEnumerable<Product>> GetAll();
-}
